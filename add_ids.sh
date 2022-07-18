9ab5d977d76467416874373b2c4827fded815d19
csae8092 <peter.andorfer@oeaw.ac.at>
2022-07-18 15:20:41 +0200

    added xml:ids and xml:base

---
+++ add_ids.sh
@@ -0,0 +1,3 @@
+add-attributes -g "./data/editions/*.xml" -b "https://csae8092.github.io/salzarchiv-static"
+add-attributes -g "./data/indices/*.xml" -b "https://csae8092.github.io/salzarchiv-static"
+add-attributes -g "./data/meta/*.xml" -b "https://csae8092.github.io/salzarchiv-static"
